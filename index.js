03f193ac1c94d28460893d72e3f7e2c3f0ae499d
Keviindran Ramachandran <keviinx@yahoo.com>
2025-08-15 15:44:52 +0200

    lint code, rename action, add icons and color for publishing

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
-const core = require('@actions/core');
-const axios = require('axios');
+const core = require("@actions/core");
+const axios = require("axios");
 
 async function run() {
   try {
-    const name = core.getInput('name');
-    const org = core.getInput('org');
-    const accessToken = core.getInput('access-token');
-    const githubAPIUrl = process.env.GITHUB_API_URL || 'https://api.github.com';
-    const visibility = core.getInput('visibility');
+    const name = core.getInput("name");
+    const org = core.getInput("org");
+    const accessToken = core.getInput("access-token");
+    const githubAPIUrl = process.env.GITHUB_API_URL || "https://api.github.com";
+    const visibility = core.getInput("visibility");
 
-    const endpoint = org ? `/orgs/${org}/repos` : '/user/repos'
-    axios.post(
-      githubAPIUrl + endpoint,
-      {
-        name,
-        visibility: visibility,
-        auto_init: true
-      },
-      {
-        headers: {
-          Authorization: 'token ' + accessToken
+    const endpoint = org ? `/orgs/${org}/repos` : "/user/repos";
+    axios
+      .post(
+        githubAPIUrl + endpoint,
+        {
+          name,
+          visibility: visibility,
+          auto_init: true,
+        },
+        {
+          headers: {
+            Authorization: "token " + accessToken,
+          },
         }
-      }
-    ).then((repository) => {
-      core.info('Repository created: ' + repository.data.html_url);
-      core.setOutput('id', repository.data.node_id);
-    }).catch(() => {
-      core.info('Repository already exists.');
-      core.setOutput('id', null);
-    })
+      )
+      .then((repository) => {
+        core.info("Repository created: " + repository.data.html_url);
+        core.setOutput("id", repository.data.node_id);
+      })
+      .catch(() => {
+        core.info("Repository already exists.");
+        core.setOutput("id", null);
+      });
   } catch (error) {
     core.setFailed(error.message);
   }
